9c071bfd12b11506192d4057f3db6b676111fdf1
xiaoyunlong <1758217010@qq.com>
2020-07-23 21:48:15 +0800

    [guessNumber][Lebron] test:should_return_2A2B_when_guess_given_guess_answer_1234_and_guess_inputnumber_1243

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -6,30 +6,31 @@ public class GuessNumber {
 
 
     public String guess(int[] guessInput, int[] answer) {
-        int countA= this.getCountOfGuessInputCorrectNumberAndPosition(guessInput,answer);
-        int countB = this.getCountOfGuessInputCorrectNumber(guessInput,answer);
-        return countA+"A"+countB+"B";
+        int countA = this.getCountOfGuessInputCorrectNumberAndPosition(guessInput, answer);
+        int countB = this.getCountOfGuessInputCorrectNumber(guessInput, answer);
+        return countA + "A" + countB + "B";
     }
 
-    public int getCountOfGuessInputCorrectNumberAndPosition(int[] guessInput,int[] answer){
+    public int getCountOfGuessInputCorrectNumberAndPosition(int[] guessInput, int[] answer) {
         int result = 0;
         for (int index = 0; index < guessInput.length; index++) {
-            if (guessInput[index] == answer[index]){
-                result ++;
+            if (guessInput[index] == answer[index]) {
+                result++;
             }
         }
-        return  result;
+        return result;
     }
 
-    public int getCountOfGuessInputCorrectNumber(int[] guessInput,int[] answer){
+    public int getCountOfGuessInputCorrectNumber(int[] guessInput, int[] answer) {
         int result = 0;
         for (int index = 0; index < guessInput.length; index++) {
-            boolean flag = guessInput[index] == answer[index];
-            if (Arrays.binarySearch(answer,guessInput[index]) >= 0 && !flag){
-                System.out.println(guessInput[index]);
-                result ++;
+            //binarySearch(Object[], int fromIndex（包含）, int toIndex(不包含), Object key) return index
+            if (Arrays.binarySearch(answer, guessInput[index]) >= 0) {
+                if (guessInput[index] != answer[index]) {
+                    result++;
+                }
             }
         }
-        return  result;
+        return result;
     }
 }
--- src/test/java/example/GuessTest.java
+++ src/test/java/example/GuessTest.java
@@ -6,51 +6,63 @@ import org.junit.jupiter.api.Test;
 public class GuessTest {
 
     @Test
-    void should_return_4A0B_when_guess_given_guess_number_1234_and_answer_1234(){
+    void should_return_4A0B_when_guess_given_guess_answer_1234_and_number_1234() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        int[] guessInput = {1,2,3,4};
-        int[] answer = {1,2,3,4};
+        int[] answer = {1, 2, 3, 4};
+        int[] guessInput = {1, 2, 3, 4};
         //when
-        String result = guessNumber.guess(guessInput,answer);
+        String result = guessNumber.guess(guessInput, answer);
         //then
-        Assertions.assertEquals("4A0B",result);
+        Assertions.assertEquals("4A0B", result);
     }
 
     @Test
-    void should_return_0A0B_when_guess_given_guess_number_1234_and_answer_5678(){
+    void should_return_0A0B_when_guess_given_guess_answer_1234_and_number_5678() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        int[] guessInput = {1,2,3,4};
-        int[] answer = {5,6,7,8};
+        int[] answer = {1, 2, 3, 4};
+        int[] guessInput = {5, 6, 7, 8};
         //when
-        String result = guessNumber.guess(guessInput,answer);
+        String result = guessNumber.guess(guessInput, answer);
         //then
-        Assertions.assertEquals("0A0B",result);
+        Assertions.assertEquals("0A0B", result);
     }
 
     @Test
-    void should_return_2A0B_when_guess_given_guess_number_1234_and_answer_1278(){
+    void should_return_2A0B_when_guess_given_guess_answer_1234_and_number_1278() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        int[] guessInput = {1,2,3,4};
-        int[] answer = {1,2,7,8};
+        int[] answer = {1, 2, 3, 4};
+        int[] guessInput = {1, 2, 7, 8};
         //when
-        String result = guessNumber.guess(guessInput,answer);
+        String result = guessNumber.guess(guessInput, answer);
         //then
-        Assertions.assertEquals("2A0B",result);
+        Assertions.assertEquals("2A0B", result);
     }
 
     @Test
-    void should_return_0A2B_when_guess_given_guess_number_1234_and_answer_7128(){
+    void should_return_0A2B_when_guess_given_guess_answer_1234_and_number_7128() {
         //given
         GuessNumber guessNumber = new GuessNumber();
-        int[] guessInput = {1,2,3,4};
-        int[] answer = {7,1,2,8};
+        int[] answer = {1, 2, 3, 4};
+        int[] guessInput = {7, 1, 2, 8};
         //when
-        String result = guessNumber.guess(guessInput,answer);
+        String result = guessNumber.guess(guessInput, answer);
         //then
-        Assertions.assertEquals("0A2B",result);
+        Assertions.assertEquals("0A2B", result);
+    }
+
+    @Test
+    void should_return_0A4B_when_guess_given_guess_answer_1234_and_number_4123() {
+        //given answer和guseeIput写反了，一直到现在才发现！！ 这就是CV的坏处！！
+        GuessNumber guessNumber = new GuessNumber();
+        int[] guessInput = {4, 1, 2, 3};
+        int[] answer = {1, 2, 3, 4};
+        //when
+        String result = guessNumber.guess(guessInput, answer);
+        //then
+        Assertions.assertEquals("0A4B", result);
     }
 
 }

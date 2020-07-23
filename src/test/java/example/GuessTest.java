db7023682b5405a652607b1ab0554a1660a1625d
xiaoyunlong <1758217010@qq.com>
2020-07-23 20:57:55 +0800

    [guessnumber][Lebron] test: should_return_0A2B_when_guess_given_guess_number_1234_and_answer_2178

--- src/test/java/example/GuessTest.java
+++ src/test/java/example/GuessTest.java
@@ -28,4 +28,17 @@ public class GuessTest {
         //then
         Assertions.assertEquals("0A0B",result);
     }
+
+    @Test
+    void should_return_2A0B_when_guess_given_guess_number_1234_and_answer_1278(){
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        int[] guessInput = {1,2,3,4};
+        int[] answer = {1,2,7,8};
+        //when
+        String result = guessNumber.guess(guessInput,answer);
+        //then
+        Assertions.assertEquals("2A0B",result);
+    }
+
 }

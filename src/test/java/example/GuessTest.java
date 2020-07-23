190ac9daf63bb96b264782e50ed20d8f066126fd
xiaoyunlong <1758217010@qq.com>
2020-07-23 22:24:48 +0800

    [guessNumber][Lebron] feature:should_return_resonable_answer_when_game_start

--- src/test/java/example/GuessTest.java
+++ src/test/java/example/GuessTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class GuessTest {
 
     @Test
-    void should_return_4A0B_when_guess_given_guess_answer_1234_and_number_1234() {
+    void should_return_4A0B_when_guess_given_guess_answer_1234_and_guess_inputnumber_1234() {
         //given
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
@@ -18,7 +18,7 @@ public class GuessTest {
     }
 
     @Test
-    void should_return_0A0B_when_guess_given_guess_answer_1234_and_number_5678() {
+    void should_return_0A0B_when_guess_given_guess_answer_1234_and_guess_inputnumber_5678() {
         //given
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
@@ -30,7 +30,7 @@ public class GuessTest {
     }
 
     @Test
-    void should_return_2A0B_when_guess_given_guess_answer_1234_and_number_1278() {
+    void should_return_2A0B_when_guess_given_guess_answer_1234_and_guess_inputnumber_1278() {
         //given
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
@@ -42,7 +42,7 @@ public class GuessTest {
     }
 
     @Test
-    void should_return_0A2B_when_guess_given_guess_answer_1234_and_number_7128() {
+    void should_return_0A2B_when_guess_given_guess_answer_1234_and_guess_inputnumber_7128() {
         //given
         GuessNumber guessNumber = new GuessNumber();
         int[] answer = {1, 2, 3, 4};
@@ -54,7 +54,7 @@ public class GuessTest {
     }
 
     @Test
-    void should_return_0A4B_when_guess_given_guess_answer_1234_and_number_4123() {
+    void should_return_0A4B_when_guess_given_guess_answer_1234_and_guess_inputnumber_4123() {
         //given answer和guseeIput写反了，一直到现在才发现！！ 这就是CV的坏处！！
         GuessNumber guessNumber = new GuessNumber();
         int[] guessInput = {4, 1, 2, 3};
@@ -63,6 +63,18 @@ public class GuessTest {
         String result = guessNumber.guess(guessInput, answer);
         //then
         Assertions.assertEquals("0A4B", result);
+    }
+
+    @Test
+    void should_return_2A2B_when_guess_given_guess_answer_1234_and_guess_inputnumber_1243() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        int[] answer = {1, 2, 3, 4};
+        int[] guessInput = {1, 2, 4, 3};
+        //when
+        String result = guessNumber.guess(guessInput, answer);
+        //then
+        Assertions.assertEquals("2A2B", result);
     }
 
 }
